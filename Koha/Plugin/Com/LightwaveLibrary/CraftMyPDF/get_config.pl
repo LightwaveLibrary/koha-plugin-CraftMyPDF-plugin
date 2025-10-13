220a2cf35099f62790be82d1ed3327ea60ad11b7
Rudy Hinojosa <rudyhinojosa2007@gmail.com>
2025-10-13 12:16:57 -0500

    Update get_config.pl

--- Koha/Plugin/Com/LightwaveLibrary/CraftMyPDF/get_config.pl
+++ Koha/Plugin/Com/LightwaveLibrary/CraftMyPDF/get_config.pl
@@ -1,21 +1,29 @@
 #!/usr/bin/perl
 
-use strict;
-use warnings;
-use CGI qw(:standard);
-use JSON;
+use Modern::Perl;
+use CGI;
+use JSON qw(encode_json decode_json);
+use C4::Context;
 
-# Always output JSON stub
-print header('application/json');
-
-my %config = (
-    plugin_name => 'Lightwave CraftMyPDF',
-    author      => 'Rudy Hinojosa / Lightwave Library',
-    version     => '1.0',
-    notes       => 'This is a placeholder for CraftMyPDF plugin configuration'
-);
-
-print encode_json(\%config);
-
-# Always exit 1
-1;
+my $cgi = CGI->new;
+my $report_id = $cgi->param('id') || '';
+my $dbh = C4::Context->dbh;
+my $query = "SELECT plugin_value FROM plugin_data WHERE plugin_class = ? AND plugin_key = ?";
+my $sth = $dbh->prepare($query);
+$sth->execute('Koha::Plugin::Com::LightwaveLibrary::CraftMyPDF', 'config');
+my ($config_json) = $sth->fetchrow_array;
+$config_json ||= '[]';
+my $configs = eval { decode_json($config_json); };
+if ($@) {
+    warn "Invalid JSON in plugin_data: $@";
+    $configs = [];
+}
+my $config = {};
+foreach my $c (@$configs) {
+    if ($c->{report_id} && $c->{report_id} eq $report_id) {
+        $config = $c;
+        last;
+    }
+}
+print $cgi->header(-type => 'application/json', -charset => 'utf-8');
+print encode_json($config);
